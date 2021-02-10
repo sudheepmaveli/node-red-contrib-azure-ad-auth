d2e76c62dd8d06f6825f500cb88712e5721e8e8d
sudheepmaveli <sudheepmaveli@gmail.com>
2021-02-10 22:37:45 +0530

    Update azure-ad-auth.js

--- azure-auth/azure-ad-auth.js
+++ azure-auth/azure-ad-auth.js
@@ -7,28 +7,26 @@ module.exports = function(RED) {
 		// Retrieve the config node
 		//azure-ad config
 		this.url= n.url;
-		var redirectUrl= n.redirectUrl;
+		node.redirectUrl= n.redirectUrl;
 		const configNode = RED.nodes.getNode(n.config);
 		const pca = configNode.pca;
-		var clientId = configNode.clientId;
-		var clientSecret = configNode.clientSecret;
+		node.clientId = configNode.clientId;
+		node.clientSecret = configNode.clientSecret;
 
 		// Create Express App and Routes
 		const authCodeUrlParameters = {
 			scopes: ["user.read"],
-			redirectUri: redirectUrl
+			redirectUri: node.redirectUrl
 		};
 
 
 
 		RED.httpNode.get(this.url, function(req, res){
 			pca.getAuthCodeUrl(authCodeUrlParameters).then(function(response) {
-				var responseData={};
-				responseData.clientId = clientId;
-				responseData.clientSecret = clientSecret;
-				responseData.redirectUrl = redirectUrl;
-				responseData.response=response;				
-				res.redirect(responseData);
+				response.clientId = node.clientId;
+				response.clientSecret = node.clientSecret;
+				response.redirectUrl = node.redirectUrl;			
+				res.redirect(response);
 			});
 
 
